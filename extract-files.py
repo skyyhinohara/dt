1f916da1d04c0d86c7494fa5c731d7a2a11ba5c6
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    surya: Add vendor soong imports to extract
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -15,6 +15,18 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/xiaomi/surya',
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8150',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/android.hardware.camera.provider@2.4-legacy.so': blob_fixup()
         .add_needed('libcamera_provider_shim.so'),
@@ -28,6 +40,7 @@ module = ExtractUtilsModule(
     'surya',
     'xiaomi',
     blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
 )
 if __name__ == '__main__':
     utils = ExtractUtils.device(module)
