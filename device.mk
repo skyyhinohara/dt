ede77c26da153ae0a834f6da49f3140280369221
HinohArata <mmgcntk@gmail.com>
2024-04-24 20:30:18 +0700

    surya: Import Apn's configs
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -84,6 +84,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+    
+# Apn
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
