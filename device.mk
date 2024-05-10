135f8afcdf2b35d77ab50f1a281465e3943267a7
Richard Raya <rdxzv.dev@gmail.com>
2024-05-10 18:17:59 -0300

    surya: Build libaudio-resampler
    Change-Id: I3822858751b9c4039e668c5c860d982ba0e12337
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -45,6 +45,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
+    libaudio-resampler \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl
 
