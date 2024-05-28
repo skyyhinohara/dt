c911b9baf55ff3787a3f636713518cd17e6babd1
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    surya: Allow to access bootable/deprecated-ota targets
    surya is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to surya namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -435,6 +435,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
