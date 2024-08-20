cb894556724ffa850e6c6f924d96757633cbae14
HinohArata <mmgcntk@gmail.com>
2024-08-21 02:28:34 +0700

    surya: Inherit MiuiCamera if exists
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -131,6 +131,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 # Camera
+$(call inherit-product-if-exists, device/xiaomi/surya-miuicamera/config.mk)
+
 PRODUCT_PACKAGES += \
     libcamera_provider_shim \
     libui_shim.vendor \
