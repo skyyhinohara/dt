03ab5b7bd035607bef9a9050d8eb812a77d1890d
LuK1337 <priv.luk@gmail.com>
2025-01-11 11:17:01 +0100

    surya: Migrate to extract-utils-py
    * Use external repo for firmware because
    python extract utils don't have feature to
    extract firmware files.
    
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
 
+# Surya Firmware
+$(call inherit-product, vendor/xiaomi/surya-firmware/Android.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
---
+++ extract-files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/android.hardware.camera.provider@2.4-legacy.so': blob_fixup()
+        .add_needed('libcamera_provider_shim.so'),
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'surya',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+)
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
