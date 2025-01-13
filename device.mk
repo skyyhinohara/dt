2b8c571668af88dcd34d1c16673bc8c0329c2358
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    surya: Migrate mount point creation out of Android.mk
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -373,6 +373,12 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
