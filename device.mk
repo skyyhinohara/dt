1c595a31a2dd876be76e9ae408e48748da34a236
HinohArata <mmgcntk@gmail.com>
2025-03-01 16:30:28 +0000

    surya: Ship Every builds with keys if available
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # XiaomiParts
 $(call inherit-product, device/xiaomi/surya/parts/parts.mk)
 
+# Signing
+-include vendor/private-keys/keys/keys.mk
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
