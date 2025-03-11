17c5e2d08f18ec5d1c598a2caf0fbb47bcad63cc
HinohArata <mmgcntk@gmail.com>
2025-03-11 12:51:02 +0000

    surya: Ship build with GalleryGO if exist
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # XiaomiParts
 $(call inherit-product, device/xiaomi/surya/parts/parts.mk)
 
+# GalleryGO
+$(call inherit-product-if-exists, vendor/prebuilts/GalleryGO/config.mk)
+
 # Kernel vintf
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
