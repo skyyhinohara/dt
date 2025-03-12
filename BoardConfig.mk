999732d07051bd0e76b65f660f2685f3f32e8373
HinohArata <mmgcntk@gmail.com>
2025-03-12 22:21:46 +0700

    surya: Nuke Torch Control
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.sm6150
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
