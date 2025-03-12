4b1609f5c039ff9051882515a5a888975f1779ce
HinohArata <mmgcntk@gmail.com>
2025-03-12 23:14:33 +0700

    Revert "surya: Build recovery QTI health AIDL service"
    This reverts commit 828992e0653d1c546be65a39d61a06c96b42594f.

--- device.mk
+++ device.mk
@@ -245,8 +245,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health-service.qti
 
 # HIDL
 PRODUCT_PACKAGES += \
