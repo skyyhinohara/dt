2becdc8e782f6498a1d1b09b9c70aeac47041a0e
HinohArata <mmgcntk@gmail.com>
2025-03-12 23:19:11 +0700

    Revert "surya: Move to QTI health AIDL service"
    This reverts commit 197f14a889d3a0b16e59884dafa9da8394489153.

--- device.mk
+++ device.mk
@@ -245,7 +245,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
