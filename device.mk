038e2bc7f82f1225009569d7c81f5112639ddd86
HinohArata <mmgcntk@gmail.com>
2025-03-12 23:13:13 +0700

    Revert "surya: Move to pixel power HAL"
    This reverts commit 054a4ccf7c45f1f5e4adac9347c5e0284b889c81.

--- device.mk
+++ device.mk
@@ -389,13 +389,8 @@ PRODUCT_BOARD_PLATFORM := $(MSMSTEPPE)
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
-
-# Enable adpf cpu hint session for SurfaceFlinger and HWUI
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_adpf_cpu_hint=true \
-    debug.hwui.use_hint_manager=true
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -462,6 +457,7 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
