7344b2e214c3bf703747f8ee3623c239777e2fa2
HinohArata <mmgcntk@gmail.com>
2025-04-19 15:02:26 +0000

    surya: Bringup to InfinityX Project
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- infinity_surya.mk
+++ infinity_surya.mk
@@ -13,9 +13,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Infinity Stuffs
+INFINITY_BUILD_TYPE := COMMUNITY
+INFINITY_MAINTAINER := Skyy丨アラタ
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := false
+WITH_GAPPS := true
+TARGET_SHIPS_FULL_GAPPS := true
+USE_MOTO_CALCULATOR := false
+
+# Product Manufacturer
+PRODUCT_NAME := infinity_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
