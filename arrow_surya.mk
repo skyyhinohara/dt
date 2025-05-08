e5bc87460c13109aafb58f5f628b279db2deadd0
HinohArata <mmgcntk@gmail.com>
2025-05-08 16:59:07 +0700

    surya: Bringup to `ArrowOS-Extended`
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- arrow_surya.mk
+++ arrow_surya.mk
@@ -12,9 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_surya
+# ArrowOS Flags
+ARROW_MAINTAINER := Skyy丨アラタ
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Additional
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Product Manufacturer
+PRODUCT_NAME := arrow_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
